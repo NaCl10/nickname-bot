641351ea29f1eb902f9af2c2f3d0d4484a51d513
NaCl10 <nacl10gaming@gmail.com>
2022-09-17 17:27:46 -0500

    forgot to commit and wrote all of v2
    in one afternoon
    
    someone kill me already

--- main.py
+++ main.py
@@ -2,71 +2,75 @@
 
 import configparser
 from os import path
-import re
-from discord.ext import commands
 import discord
-from string import Template
+from discord import app_commands
+import traceback
 
-# Read config
+TESTING_GUILD = discord.Object(id=593483503241396285)
+
+# Config
 config = configparser.ConfigParser()
 if not path.isfile('config.ini'):
     config['config'] = {'status': 'Being bad at changing nicknames',
             'token': ''}
-    config['prefixes'] = {}
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 config.read('config.ini')
 
-async def get_prefix(bot, message):
-    '''Check the prefix for the guild the command is in. The commands module will run this function every time a message is recieved and it checks if it's a command.'''
-    guild = str(message.guild.id)
-    if guild in config['prefixes']:
-        return config['prefixes'][guild]
-    else:
-        return '%'
+class CommandsClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # Command tree for the app
+        self.tree = app_commands.CommandTree(self)
 
-client = commands.Bot(command_prefix=get_prefix, help_command=None)
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=TESTING_GUILD)
+        await self.tree.sync(guild=TESTING_GUILD)
+
+class ChangeNickname(discord.ui.Modal, title="Change Nickname"):
+    def __init__(self, *, member: discord.Member, interaction: discord.Interaction):
+        super().__init__(custom_id = "changenickname")
+        self.member = member
+        self.interaction = interaction
+        self.add_item(discord.ui.TextInput(label="Nickname", placeholder=self.member.name, default=self.member.nick, required=False, max_length=32))
+
+    async def on_submit(self, interaction: discord.Interaction):
+        try:
+            await self.member.edit(nick=str(self.children[0].value), reason='Action requested by ' + str(self.interaction.user))
+        except discord.errors.Forbidden:
+            await interaction.response.send_message('Looks like I don\'t have permission to change that user\'s nickname! Please ensure that my role is above     that user\'s highest role in the role list and that I have the "Manage Nicknames" permission.', ephemeral=True)
+        else:
+            await interaction.response.send_message('Nickname successfully changed!', ephemeral=True)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('An unknown error ocurred changing that user\'s nickname.', ephemeral=True)
+        traceback.print_tb(error.__traceback__)
+        print(error)
+
+intents=discord.Intents.default()
+client=CommandsClient(intents=intents)
 
 @client.event
 async def on_ready():
-    '''Set status and log that we logged in'''
-    print('Logged in as {0.user}'.format(client))
+    print(f'Logged in as {client.user}')
     await client.change_presence(activity=discord.Game(config['config']['status']))
 
-@client.command(pass_context=True)
-async def help(ctx):
-    '''Help command'''
-    embed = discord.Embed(title='Help',
-            color=discord.Color(0x000000))
-    embed.add_field(name='help', value='Shows this help message.', inline=False)
-    embed.add_field(name='ping', value='Checks if the bot is online and working and displays bot latency', inline=False)
-    embed.add_field(name='prefix', value='Usage: `prefix <prefix>`\nSets `<prefix>` to be the new prefix in this server.\nNOTE: Administrator only.', inline=False)
-    embed.add_field(name='nickname', value='Usage: `nickname <user id/mention> <new nickname>`\n`<user id/mention>` should be the user ID or @mention of a user **in this server** whose nickname you would like to change. <new nickname> should be what you would like to set their nickname to.', inline=False)
-    embed.set_footer(text='prefix: ' + await get_prefix(bot=client, message=ctx))
-    await ctx.send(embed=embed)
+@client.tree.command()
+async def help(interaction: discord.Interaction):
+    """Displays the bot's help, containing a list of commands and how to change a user's nickname."""
+    embed = discord.Embed(title='Help', color=discord.Color(0x000000))
+    embed.add_field(name="To change ***any*** user's nickname:", value="Right-click on the user, then go to apps > change nickname. (Or on mobile, tap on the user, then go to apps > change nickname). From there, a dialog should pop up that will allow you to enter a new nickname. Leave it blank to remove their nickname. Once you're done, click submit.", inline=False)
+    embed.add_field(name="/help", value="Displays this help message.", inline=True)
+    embed.add_field(name="/ping", value="Checks if the bot is online and displays its latency.", inline=True)
+    await interaction.response.send_message(embed=embed)
 
-@client.command(pass_context=True)
-async def ping(ctx):
-    '''Send "Pong! (bot latency)" in the channel the command was run in'''
-    await ctx.send('Pong! Latency: {0}ms'.format(round(client.latency * 1000, 1)))
+@client.tree.command()
+async def ping(interaction: discord.Interaction):
+    """Checks if the bot is online and displays its latency."""
+    await interaction.response.send_message('Pong! Latency: {0}ms'.format(round(client.latency * 1000, 1)))
 
-@client.command(pass_context=True)
-@commands.has_permissions(administrator=True)
-async def prefix(ctx, prefix):
-    '''Change the bot's prefix in the current guild'''
-    config['prefixes'][str(ctx.guild.id)] = prefix.replace('%', '%%') # Replace % with %% because % is a special character in the config library
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-
-    # We have to use the Template method here because f-strings and such may allow for abitrary variable retrieval by users (see https://realpython.com/python-string-formatting/#4-template-strings-standard-library)
-    response = Template('Prefix changed to $prefix')
-    await ctx.send(response.substitute(prefix=prefix))
-
-@client.command(pass_context = True)
-async def nickname(ctx, user: discord.Member, *, nickname):
-    try:
-        await user.edit(nick=nickname, reason='Action requested by ' + str(ctx.author) + ' fulfilled by ' + str(client.user))
-    except discord.errors.Forbidden:
-        await ctx.send('Looks like I don\'t have permission to change that user\'s nickname! Please ensure that my role is above that user\'s highest role in the role list and that I have the "Manage Nicknames" permission.')
+@client.tree.context_menu(name="Change Nickname")
+async def change_nickname(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.send_modal(ChangeNickname(member=member, interaction=interaction))
 
 client.run(config['config']['token'])
